069dcd34c901dfb137d3f436e930b54a96f1574a
Jared-sta <fastracquet@gmail.com>
2024-04-25 22:34:09 -0600

    Fixed doubles stay in method call of movePlayer. Community Chest and Chance methods split up for accurate functionality.

--- src/monopoly/Monopoly.java
+++ src/monopoly/Monopoly.java
@@ -15,10 +15,32 @@ public class Monopoly {
     private Stack<String> chanceCards = new Stack<>();
 
     public Monopoly() {
-        loadCards();
+        loadCommunityCards();
+		loadChanceCards();
     }
 
-    private void loadCards() {
+	private void loadChanceCards() {
+		if (!getOutOfJailFreeChance){
+			chanceCards.push("Get Out of Jail Free");
+		}
+        chanceCards.push("Advance to Boardwalk");
+		chanceCards.push("Advance to Go");
+		chanceCards.push("Advance to Illinois Avenue");
+		chanceCards.push("Advance to St. Charles Place");
+		chanceCards.push("Advance to nearest Railroad");
+		chanceCards.push("Advance to nearest Railroad");
+		chanceCards.push("Advance to nearest Utility");
+		chanceCards.push("Go Back 3 Spaces");
+		chanceCards.push("Go to Jail");
+		chanceCards.push("Take a trip to Reading Railroad");
+		for (int i = 0; i < 5; i++){
+			chanceCards.push("NA");
+		}
+
+        Collections.shuffle(chanceCards);
+	}
+
+    private void loadCommunityCards() {
 		if (!getOutOfJailFreeCommunity){
 			communityChestCards.push("Get out of Jail Free");
 		}
@@ -29,85 +51,81 @@ public class Monopoly {
 		}
 
         Collections.shuffle(communityChestCards);
-
-		if (!getOutOfJailFreeChance){
-			chanceCards.push("Get Out of Jail Free");
-		}
-        chanceCards.push("Advance to Boardwalk");
-		chanceCards.push("Advance to Go");
-		chanceCards.push("Advance to Illinois Avenue");
-		chanceCards.push("Advance to St. Charles Place");
-		chanceCards.push("Advance to the nearest Railroad");
-		chanceCards.push("Advance to the nearest Railroad");
-		chanceCards.push("Advance token to nearest Utility");
-		chanceCards.push("Go Back 3 Spaces");
-		chanceCards.push("Go to Jail");
-		chanceCards.push("Take a trip to Reading Railroad");
-		for (int i = 0; i < 5; i++){
-			chanceCards.push("NA");
-		}
-
-        Collections.shuffle(chanceCards);
     }
 
     public void movePlayer() {
-        DiceResult diceResult = dice.RollDice();
+		DiceResult diceResult;
+		boolean releasedFromJail = false;
+		do {
+        	diceResult = dice.RollDice();
 
-		if (diceResult.getConsecutiveDoublesCount() == 3){
-			goToJail();
-			return;
-		}
-
-        if (inJail) {
-            handleJailA(diceResult);
-			if (inJail){
-				turnsInJail++;
-				return; // Onlly return if player did not get out after handleJail
+			if (diceResult.getConsecutiveDoublesCount() == 3){
+				diceResult.set_consecutiveDoublesCount(0);
+				goToJail();
+				return;
 			}
-            diceResult.set_consecutiveDoublesCount(0);
-        }
 
-        int spacesToMove = diceResult.getDiceRoll1() + diceResult.getDiceRoll2();
-        currentPosition = (currentPosition + spacesToMove) % 40;
-        board[currentPosition]++;
+       	 	if (inJail) {
+            	releasedFromJail = handleJailA(diceResult);
+				if (inJail){
+					turnsInJail++;
+					return; // Onlly return if player did not get out after handleJail
+				}	
+				diceResult.set_consecutiveDoublesCount(0);
+       	 	}
 
-        if (currentPosition == 30) {
-            goToJail();
-        } else {
-            checkLandingPosition();
-        }
+        	int spacesToMove = diceResult.getDiceRoll1() + diceResult.getDiceRoll2();
+        	currentPosition = (currentPosition + spacesToMove) % 40;
+       	 	board[currentPosition]++;
+
+        	if (currentPosition == 30) {
+           		goToJail();
+        	} else {
+            	checkLandingPosition();
+        	}
+
+		 	if (releasedFromJail) {
+		 		return;
+			}	
+		 	
+		} while (diceResult.isDoubles());
     }
 
-    private void handleJailB(DiceResult diceResult) {
+    private boolean handleJailB(DiceResult diceResult) {
         if (getOutOfJailFreeChance) {
             getOutOfJailFreeChance = false;
             inJail = false;
-            return;
+            return true;
         } else if (getOutOfJailFreeCommunity) {
 			getOutOfJailFreeCommunity = false;
             inJail = false;
-            return;
+            return true;
 		}
         if (diceResult.isDoubles()) {
             inJail = false;
+			return true;
         }
 		if (turnsInJail == 3){
-			turnsInJail = 0;
 			inJail = false;
+			return true;
 		}
+		return false;
     }
 
 	// player gets out of jail either way for strategy A
-	private void handleJailA(DiceResult diceResult) {
+	private boolean handleJailA(DiceResult diceResult) {
         if (getOutOfJailFreeChance) {
             getOutOfJailFreeChance = false;
         } else if (getOutOfJailFreeCommunity) {
 			getOutOfJailFreeCommunity = false;
+
 		}
         inJail = false;
+		return true;
     }
 
     private void goToJail() {
+		turnsInJail = 0;
         currentPosition = 10;
         inJail = true;
     }
@@ -117,28 +135,60 @@ public class Monopoly {
             case 2:
             case 17:
             case 33:
-                drawCard(communityChestCards);
+                drawCommunityCard(communityChestCards);
                 break;
             case 7:
             case 22:
             case 36:
-                drawCard(chanceCards);
+                drawChanceCard(chanceCards);
                 break;
         }
     }
 
-    private void drawCard(Stack<String> cards) {
+    private void drawCommunityCard(Stack<String> cards) {
         if (cards.isEmpty()) {
-            loadCards(); 
+            loadCommunityCards(); 
         }
         String card = cards.pop();
-        executeCardAction(card);
+        executeCommunityCardAction(card);
     }
 
-    private void executeCardAction(String card) {
-        //System.out.println("Card drawn: " + card);
-        // TODO
+    private void executeCommunityCardAction(String card) {
+		switch (card) {
+			case "Advance to Go":
+				currentPosition = 0;
+				break;
+			case "Go to Jail":
+				goToJail();
+				break;
+			case "Get Out of Jail Free":
+				getOutOfJailFreeCommunity = true;
+				break;
+		}
     }
+
+	private void drawChanceCard(Stack<String> cards) {
+        if (cards.isEmpty()) {
+            loadChanceCards(); 
+        }
+        String card = cards.pop();
+        executeChanceCardAction(card);
+    }
+
+	private void executeChanceCardAction(String card) {
+		switch (card) {
+			case "Advance to Go":
+				currentPosition = 0;
+				break;
+			case "Go to Jail":
+				goToJail();
+				break;
+			case "Get Out of Jail Free":
+				getOutOfJailFreeChance = true;
+				break;
+			// TODO - finish chance cases
+		}
+	}
 
     public static void main(String[] args) {
         Monopoly game = new Monopoly();
